d9a37098b0ee24c38eb2b94614a886a3d7104ef4
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-08 09:51:08 +0200

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -203,6 +203,11 @@ class FreeplayState extends MusicBeatState
 		text.setFormat(Paths.font("vcr.ttf"), size, FlxColor.WHITE, RIGHT);
 		text.scrollFactor.set();
 		add(text);
+
+                #if android
+                addVirtualPad(FULL, A_B_C_X_Y);
+                #end
+
 		super.create();
 	}
 
